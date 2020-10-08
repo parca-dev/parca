dc7b84ac2cf6b17a9777ab743f6b850cb3017c92
Frederic Branczyk <fbranczyk@gmail.com>
2020-10-08 15:45:26 +0200

    api: Add top report type support to query API

--- api/api.go
+++ api/api.go
@@ -181,11 +181,78 @@ func (a *API) Query(r *http.Request) (interface{}, []error, *ApiError) {
 	}
 
 	switch r.URL.Query().Get("report") {
+	case "top":
+		rep, err := generateTopReport(profile)
+		if err != nil {
+			return nil, nil, &ApiError{Typ: ErrorExec, Err: err}
+		}
+		return rep, nil, nil
 	case "svg":
 		return &svgRenderer{profile: profile}, nil, nil
 	default:
 		return &svgRenderer{profile: profile}, nil, nil
 	}
+}
+
+type textItem struct {
+	Name        string `json:"name,omitempty"`
+	InlineLabel string `json:"inlineLabel,omitempty"`
+	Flat        int64  `json:"flat,omitempty"`
+	Cum         int64  `json:"cum,omitempty"`
+	FlatFormat  string `json:"flatFormat,omitempty"`
+	CumFormat   string `json:"cumFormat,omitempty"`
+}
+
+type topReport struct {
+	Labels []string   `json:"labels,omitempty"`
+	Items  []textItem `json:"items,omitempty"`
+}
+
+func generateTopReport(p *profile.Profile) (*topReport, error) {
+	numLabelUnits, _ := p.NumLabelUnits()
+	p.Aggregate(false, true, true, true, false)
+
+	value, meanDiv, sample, err := sampleFormat(p, "", false)
+	if err != nil {
+		return nil, err
+	}
+
+	stype := sample.Type
+
+	rep := report.NewDefault(p, report.Options{
+		OutputFormat:  report.Text,
+		OutputUnit:    "minimum",
+		Ratio:         1,
+		NumLabelUnits: numLabelUnits,
+
+		SampleValue:       value,
+		SampleMeanDivisor: meanDiv,
+		SampleType:        stype,
+		SampleUnit:        sample.Unit,
+
+		NodeCount:    500,
+		NodeFraction: 0.005,
+		EdgeFraction: 0.001,
+	})
+
+	items, labels := report.TextItems(rep)
+	res := &topReport{
+		Labels: labels,
+		Items:  make([]textItem, 0, len(items)),
+	}
+
+	for _, i := range items {
+		res.Items = append(res.Items, textItem{
+			Name:        i.Name,
+			InlineLabel: i.InlineLabel,
+			Flat:        i.Flat,
+			Cum:         i.Cum,
+			FlatFormat:  i.FlatFormat,
+			CumFormat:   i.CumFormat,
+		})
+	}
+
+	return res, nil
 }
 
 type svgRenderer struct {
