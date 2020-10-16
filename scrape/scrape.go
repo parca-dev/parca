83b099f595db09fa1cca35cce630d6980f9373b1
Frederic Branczyk <fbranczyk@gmail.com>
2020-10-16 08:16:56 +0200

    scrape: Always gzip compress individual samples

--- scrape/scrape.go
+++ scrape/scrape.go
@@ -393,9 +393,22 @@ func (s *targetScraper) scrape(ctx context.Context, w io.Writer, profileType str
 		}
 
 	default:
-		_, err = profile.Parse(io.TeeReader(resp.Body, w))
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return errors.Wrap(err, "failed to read body")
+		}
+
+		p, err := profile.ParseData(b)
 		if err != nil {
 			return errors.Wrap(err, "failed to parse target's pprof profile")
+		}
+
+		if len(p.Sample) == 0 {
+			return fmt.Errorf("empty %s profile from %s", profileType, s.req.URL.String())
+		}
+
+		if err := p.Write(w); err != nil {
+			return fmt.Errorf("write profile: %w", err)
 		}
 	}
 
