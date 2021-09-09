cff5068c2250efc7dd076fcbae465748d59c8354
Simon Wicki <simon.wicki@justwatch.com>
2021-09-09 10:07:12 +0200

    fix: navbar full width

--- ui/packages/app/web/src/components/ui/Navbar.tsx
+++ ui/packages/app/web/src/components/ui/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     <Disclosure as='nav' className='bg-gray-800 relative z-10'>
       {({ open }) => (
         <>
-          <div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 xl:px-0'>
+          <div className='mx-auto px-3 sm:px-6 lg:px-8 xl:px-0'>
             <div className='relative flex items-center justify-between h-16'>
               <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
                 {/* mobile menu button */}
