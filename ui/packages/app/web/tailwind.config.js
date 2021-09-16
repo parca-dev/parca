245f724d833312198a1bc3cd72e7281464290da4
Simon Wicki <simon.wicki@justwatch.com>
2021-09-16 13:03:56 +0200

    fix: too abrasive css purging

--- ui/packages/app/web/tailwind.config.js
+++ ui/packages/app/web/tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     enabled: process.env.NODE_ENV === 'production',
-    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}']
+    content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}']
   },
   darkMode: 'class',
   theme: {},
