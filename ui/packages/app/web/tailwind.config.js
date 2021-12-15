f4f2f1c31a6959cf6754590b87c3520fff4742ce
Manoj Vivek <p.manoj.vivek@gmail.com>
2021-12-15 11:52:53 +0530

    Tailwind config file ported to v3

--- ui/packages/app/web/tailwind.config.js
+++ ui/packages/app/web/tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}']
-  },
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {},
   variants: {},
