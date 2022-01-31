43902d6fe67af448c8845048becd0eb52d09d553
Manoj Vivek <p.manoj.vivek@gmail.com>
2022-01-31 11:56:11 +0530

    Added support for passing className prop to Select component

--- ui/packages/app/web/src/components/ui/Select.tsx
+++ ui/packages/app/web/src/components/ui/Select.tsx
@@ -26,12 +26,14 @@ const Select = ({
   onSelection,
   placeholder,
   width,
+  className,
 }: {
   items: SelectItem[];
   selectedKey: string | undefined;
   onSelection: (value: string | undefined) => void;
   placeholder?: string;
   width?: number;
+  className?: string;
 }): JSX.Element => {
   const selection = items.find(v => v.key === selectedKey) ?? {
     key: selectedKey,
@@ -47,7 +49,8 @@ const Select = ({
               <Listbox.Button
                 className={cx(
                   width !== undefined ? `w-${width}` : '',
-                  'relative bg-gray-50 dark:bg-gray-900 border-t border-r border-b border-l dark:border-gray-600 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+                  'relative bg-gray-50 dark:bg-gray-900 border-t border-r border-b border-l dark:border-gray-600 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm',
+                  {[className as string]: !!className}
                 )}
               >
                 <span className="flex items-center">
