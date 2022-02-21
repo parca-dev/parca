ae98b6db91bf23ae7c11a822d1fb4bd0cc8bf5f1
thorfour <thor.hansen@polarsignals.com>
2022-02-21 14:57:21 -0600

    some more unit tests

--- pkg/columnstore/list/list_test.go
+++ pkg/columnstore/list/list_test.go
@@ -1,12 +1,14 @@
 package list
 
 import (
-	"fmt"
+	"sync"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func Test_SingleList(t *testing.T) {
-	l := List{}
+	l := &List{}
 
 	for i := 0; i < 10; i++ {
 		l.Prepend(&Node{
@@ -14,8 +16,37 @@ func Test_SingleList(t *testing.T) {
 		})
 	}
 
+	j := 9
 	l.Iterate(func(i int) bool {
-		fmt.Println(i)
+		require.Equal(t, j, i)
+		j--
 		return true
 	})
+}
+
+func Test_List_Concurrent(t *testing.T) {
+
+	l := &List{}
+
+	wg := &sync.WaitGroup{}
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			for i := 0; i < 10; i++ {
+				l.Prepend(&Node{
+					part: i,
+				})
+			}
+		}(i)
+	}
+	wg.Wait()
+
+	total := 0
+	l.Iterate(func(i int) bool {
+		total += i
+		return true
+	})
+
+	require.Equal(t, 45*10, total)
 }
