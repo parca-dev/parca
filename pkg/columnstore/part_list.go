ab983cc6f49c5f7552c3ce3ff705769fc551614b
thorfour <thor.hansen@polarsignals.com>
2022-02-21 21:09:45 -0600

    added way to add sentinel

--- pkg/columnstore/part_list.go
+++ pkg/columnstore/part_list.go
@@ -5,12 +5,12 @@ import (
 	"unsafe"
 )
 
-type sentinel uint8
+type SentinelType uint8
 
 const (
-	none sentinel = iota
-	compacting
-	compacted
+	None SentinelType = iota
+	Compacting
+	Compacted
 )
 
 // Node is a Part that is a part of a linked-list
@@ -18,12 +18,27 @@ type Node struct {
 	next unsafe.Pointer
 	part *Part
 
-	sentinel sentinel // sentinel nodes contain no parts, and are to indicate the start of a new sub list
+	sentinel SentinelType // sentinel nodes contain no parts, and are to indicate the start of a new sub list
 }
 
 type PartList struct {
 	next  unsafe.Pointer
 	total uint64
+}
+
+// Sentinel adds a new sentinel node to the list
+func (l *PartList) Sentinel(s SentinelType) {
+	node := &Node{
+		sentinel: s,
+	}
+	for { // continue until a successful compare and swap occurs
+		next := atomic.LoadPointer(&l.next)
+		node.next = next
+		if atomic.CompareAndSwapPointer(&l.next, next, (unsafe.Pointer)(node)) {
+			atomic.AddUint64(&l.total, 1)
+			return
+		}
+	}
 }
 
 // Prepend a node onto the front of the list
@@ -34,8 +49,8 @@ func (l *PartList) Prepend(part *Part) *Node {
 	for { // continue until a successful compare and swap occurs
 		next := atomic.LoadPointer(&l.next)
 		node.next = next
-		if (*Node)(next).sentinel == compacted { // This list is apart of a compacted granule, propogate the compacted value so each subsequent Prepend can return the correct value
-			node.sentinel = compacted
+		if (*Node)(next).sentinel == Compacted { // This list is apart of a compacted granule, propogate the compacted value so each subsequent Prepend can return the correct value
+			node.sentinel = Compacted
 		}
 		if atomic.CompareAndSwapPointer(&l.next, next, (unsafe.Pointer)(node)) {
 			atomic.AddUint64(&l.total, 1)
