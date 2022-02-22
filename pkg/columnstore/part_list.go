524b465ac9a2a88cb52b298035a0b89be599734a
thorfour <thor.hansen@polarsignals.com>
2022-02-21 21:03:55 -0600

    added sentinel nodes to part list

--- pkg/columnstore/part_list.go
+++ pkg/columnstore/part_list.go
@@ -5,10 +5,20 @@ import (
 	"unsafe"
 )
 
+type sentinel uint8
+
+const (
+	none sentinel = iota
+	compacting
+	compacted
+)
+
 // Node is a Part that is a part of a linked-list
 type Node struct {
 	next unsafe.Pointer
 	part *Part
+
+	sentinel sentinel // sentinel nodes contain no parts, and are to indicate the start of a new sub list
 }
 
 type PartList struct {
@@ -17,16 +27,19 @@ type PartList struct {
 }
 
 // Prepend a node onto the front of the list
-func (l *PartList) Prepend(part *Part) {
+func (l *PartList) Prepend(part *Part) *Node {
 	node := &Node{
 		part: part,
 	}
 	for { // continue until a successful compare and swap occurs
 		next := atomic.LoadPointer(&l.next)
 		node.next = next
+		if (*Node)(next).sentinel == compacted { // This list is apart of a compacted granule, propogate the compacted value so each subsequent Prepend can return the correct value
+			node.sentinel = compacted
+		}
 		if atomic.CompareAndSwapPointer(&l.next, next, (unsafe.Pointer)(node)) {
 			atomic.AddUint64(&l.total, 1)
-			return
+			return node
 		}
 	}
 }
