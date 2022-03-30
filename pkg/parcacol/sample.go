5f8ebb50087d0c474d1b2f8d794689f3d42f12fb
Matthias Loibl <mail@matthiasloibl.com>
2022-03-30 18:00:38 +0200

    pkg/parcacol: Use a switch-case to insert every column in the correct order

--- pkg/parcacol/sample.go
+++ pkg/parcacol/sample.go
@@ -1,6 +1,7 @@
 package parcacol
 
 import (
+	"fmt"
 	"sort"
 
 	"github.com/polarsignals/arcticdb/dynparquet"
@@ -41,7 +42,7 @@ func (s Samples) ToBuffer(schema *dynparquet.Schema) (*dynparquet.Buffer, error)
 
 	var r parquet.Row
 	for _, sample := range s {
-		r = sample.ToParquetRow(r[:0], names, pprofLabels, pprofNumLabels)
+		r = sample.ToParquetRow(schema, r[:0], names, pprofLabels, pprofNumLabels)
 		err := pb.WriteRow(r)
 		if err != nil {
 			return nil, err
@@ -102,108 +103,118 @@ func (s Samples) pprofNumLabelsNames() []string {
 	return names
 }
 
-func (s Sample) ToParquetRow(row parquet.Row, labelNames, pprofLabelNames, pprofNumLabelNames []string) parquet.Row {
-	// The order of these appends is important. Parquet values must be in the
-	// order of the schema and the schema orders columns by their names.
-	labelNamesLen := len(labelNames)
-	pprofLabelsNamesLen := len(pprofLabelNames)
-	pprofNumLabelsNamesLen := len(pprofNumLabelNames)
-	dynamicNum := labelNamesLen + pprofLabelsNamesLen + pprofNumLabelsNamesLen
+func (s Sample) ToParquetRow(schema *dynparquet.Schema, row parquet.Row, labelNames, pprofLabelNames, pprofNumLabelNames []string) parquet.Row {
+	// schema.Columns() returns a sorted list of all columns.
+	// We match on the column's name to insert the correct values.
+	// We track the columnIndex to insert each column at the correct index.
+	columnIndex := 0
+	for _, column := range schema.Columns() {
+		switch column.Name {
+		case columnDuration:
+			row = append(row, parquet.ValueOf(s.Duration).Level(0, 0, columnIndex))
+			columnIndex++
+		case columnPeriod:
+			row = append(row, parquet.ValueOf(s.Period).Level(0, 0, columnIndex))
+			columnIndex++
+		case columnPeriodType:
+			row = append(row, parquet.ValueOf(s.PeriodType).Level(0, 0, columnIndex))
+			columnIndex++
+		case columnPeriodUnit:
+			row = append(row, parquet.ValueOf(s.PeriodUnit).Level(0, 0, columnIndex))
+			columnIndex++
+		case columnSampleType:
+			row = append(row, parquet.ValueOf(s.SampleType).Level(0, 0, columnIndex))
+			columnIndex++
+		case columnSampleUnit:
+			row = append(row, parquet.ValueOf(s.SampleUnit).Level(0, 0, columnIndex))
+			columnIndex++
+		case columnStacktrace:
+			row = append(row, parquet.ValueOf(s.Stacktrace).Level(0, 0, columnIndex))
+			columnIndex++
+		case columnTimestamp:
+			row = append(row, parquet.ValueOf(s.Timestamp).Level(0, 0, columnIndex))
+			columnIndex++
+		case columnValue:
+			row = append(row, parquet.ValueOf(s.Value).Level(0, 0, columnIndex))
+			columnIndex++
 
-	if row == nil {
-		row = make([]parquet.Value, 0, 9+dynamicNum)
-	}
+		// All remaining cases take care of dynamic columns
+		case columnLabels:
+			labelNamesLen := len(labelNames)
+			i, j := 0, 0
+			for i < labelNamesLen {
+				if labelNames[i] == s.Labels[j].Name {
+					value := parquet.ValueOf(s.Labels[j].Value).Level(0, 1, columnIndex)
+					row = append(row, value)
+					columnIndex++
+					i++
+					j++
 
-	row = append(row, parquet.ValueOf(s.Duration).Level(0, 0, 0))
-
-	// Labels
-	i, j := 0, 0
-	for i < labelNamesLen {
-		columnIndex := i + 1
-
-		if labelNames[i] == s.Labels[j].Name {
-			value := parquet.ValueOf(s.Labels[j].Value).Level(0, 1, columnIndex)
-			row = append(row, value)
-			i++
-			j++
-
-			if j >= len(s.Labels) {
-				for ; i < labelNamesLen; i++ {
+					if j >= len(s.Labels) {
+						for ; i < labelNamesLen; i++ {
+							row = append(row, parquet.ValueOf(nil).Level(0, 0, columnIndex))
+						}
+						break
+					}
+				} else {
+					// If nothing matches we add a NULL to the column
 					row = append(row, parquet.ValueOf(nil).Level(0, 0, columnIndex))
+					columnIndex++
+					i++
 				}
-				break
 			}
-		} else {
-			// If nothing matches we add a NULL to the column
-			row = append(row, parquet.ValueOf(nil).Level(0, 0, columnIndex))
-			i++
+		case columnPprofLabels:
+			i, j := 0, 0
+			pprofLabelsNamesLen := len(pprofLabelNames)
+			for i < pprofLabelsNamesLen {
+				if pprofLabelNames[i] == s.PprofLabels[j].Name {
+					value := parquet.ValueOf(s.PprofLabels[j].Value).Level(0, 1, columnIndex)
+					row = append(row, value)
+					columnIndex++
+					i++
+					j++
+
+					if j >= len(s.PprofLabels) {
+						for ; i < pprofLabelsNamesLen; i++ {
+							row = append(row, parquet.ValueOf(nil).Level(0, 0, columnIndex))
+						}
+						break
+					}
+				} else {
+					// If nothing matches we add a NULL to the column
+					row = append(row, parquet.ValueOf(nil).Level(0, 0, columnIndex))
+					columnIndex++
+					i++
+				}
+			}
+		case columnPprofNumLabels:
+			i, j := 0, 0
+			pprofNumLabelsNamesLen := len(pprofNumLabelNames)
+			for i < pprofNumLabelsNamesLen {
+				if pprofNumLabelNames[i] == s.PprofNumLabels[j].Name {
+					value := parquet.ValueOf(s.PprofNumLabels[j].Value).Level(0, 1, columnIndex)
+					row = append(row, value)
+					columnIndex++
+					i++
+					j++
+
+					if j >= len(s.PprofNumLabels) {
+						for ; i < pprofNumLabelsNamesLen; i++ {
+							row = append(row, parquet.ValueOf(nil).Level(0, 0, columnIndex))
+						}
+						break
+					}
+				} else {
+					// If nothing matches we add a NULL to the column
+					row = append(row, parquet.ValueOf(nil).Level(0, 0, columnIndex))
+					columnIndex++
+					i++
+				}
+			}
+		default:
+			panic(fmt.Errorf("conversion not implement for column: %s", column.Name))
 		}
 	}
-
-	// We add these columns at their index with the initial padding of the size of dynamic columns.
-	row = append(row, parquet.ValueOf(s.Period).Level(0, 0, labelNamesLen+1))
-	row = append(row, parquet.ValueOf(s.PeriodType).Level(0, 0, labelNamesLen+2))
-	row = append(row, parquet.ValueOf(s.PeriodUnit).Level(0, 0, labelNamesLen+3))
-
-	// pprofLabels
-	i, j = 0, 0
-	for i < pprofLabelsNamesLen {
-		// add the previous labelName column index on top
-		// duration, period, period_type, period_unit are index 4 on top
-		columnIndex := labelNamesLen + i + 4
-
-		if pprofLabelNames[i] == s.PprofLabels[j].Name {
-			value := parquet.ValueOf(s.PprofLabels[j].Value).Level(0, 1, columnIndex)
-			row = append(row, value)
-			i++
-			j++
-
-			if j >= len(s.PprofLabels) {
-				for ; i < pprofLabelsNamesLen; i++ {
-					row = append(row, parquet.ValueOf(nil).Level(0, 0, columnIndex))
-				}
-				break
-			}
-		} else {
-			// If nothing matches we add a NULL to the column
-			row = append(row, parquet.ValueOf(nil).Level(0, 0, columnIndex))
-			i++
-		}
-	}
-
-	// pprofNumLabels
-	i, j = 0, 0
-	for i < pprofNumLabelsNamesLen {
-		// add the previous labelNames and pprofLabelsNames column index on top
-		// duration, period, period_type, period_unit are index 4 on top
-		columnIndex := labelNamesLen + pprofLabelsNamesLen + i + 4
-
-		if pprofNumLabelNames[i] == s.PprofNumLabels[j].Name {
-			value := parquet.ValueOf(s.PprofNumLabels[j].Value).Level(0, 1, columnIndex)
-			row = append(row, value)
-			i++
-			j++
-
-			if j >= len(s.PprofNumLabels) {
-				for ; i < pprofNumLabelsNamesLen; i++ {
-					row = append(row, parquet.ValueOf(nil).Level(0, 0, columnIndex))
-				}
-				break
-			}
-		} else {
-			// If nothing matches we add a NULL to the column
-			row = append(row, parquet.ValueOf(nil).Level(0, 0, columnIndex))
-			i++
-		}
-	}
-
-	// the indexes start at dynamicNum which is the sum of all dynamic columns.
-	// on top of that start with the 4 for: duration, period, period_type, period_unit
-	row = append(row, parquet.ValueOf(s.SampleType).Level(0, 0, dynamicNum+4))
-	row = append(row, parquet.ValueOf(s.SampleUnit).Level(0, 0, dynamicNum+5))
-	row = append(row, parquet.ValueOf(s.Stacktrace).Level(0, 0, dynamicNum+6))
-	row = append(row, parquet.ValueOf(s.Timestamp).Level(0, 0, dynamicNum+7))
-	row = append(row, parquet.ValueOf(s.Value).Level(0, 0, dynamicNum+8))
 
 	return row
 }
