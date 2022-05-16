e051d5d4c6183ca98f803bb25a5f4afe3a7edfd0
Kautilya Tripathi <tripathi.kautilya@gmail.com>
2022-05-16 11:31:16 +0530

    env-local-test: ctlptl update to latest version
    Check https://github.com/tilt-dev/ctlptl/releases/tag/v0.8.1 for more
    information about the release.
    
    Signed-off-by: Kautilya Tripathi <tripathi.kautilya@gmail.com>

--- env-local-test.sh
+++ env-local-test.sh
@@ -14,5 +14,5 @@ sudo mv "minikube-linux-$ARCH" /usr/local/bin/minikube
 
 curl -fsSL https://raw.githubusercontent.com/tilt-dev/tilt/master/scripts/install.sh | bash
 
-CTLPTL_VERSION="0.5.1"
+CTLPTL_VERSION="0.8.1"
 curl -fsSL "https://github.com/tilt-dev/ctlptl/releases/download/v$CTLPTL_VERSION/ctlptl.$CTLPTL_VERSION.linux.$(if [ $ARCH=amd64 ]; then echo "$ARCH_UNAME"; else echo "$ARCH"; fi).tar.gz" | sudo tar -xzv -C /usr/local/bin ctlptl
