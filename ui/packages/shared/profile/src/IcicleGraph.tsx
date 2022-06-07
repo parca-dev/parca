f45134a1f37cdbf932487d79860164a2ba5b367c
Yomi Eluwande <yomi.eluwande@gmail.com>
2022-06-07 15:20:58 +0200

    area/ui: Fix the Icicle graph bug for Firefox browsers
    I modified the IcicleGraph's component svg element to get it's width & height from the respective width and height propoerty as opposed to using the `viewBox`

--- ui/packages/shared/profile/src/IcicleGraph.tsx
+++ ui/packages/shared/profile/src/IcicleGraph.tsx
@@ -521,8 +521,9 @@ export default function IcicleGraph({
       />
       <svg
         className="font-robotoMono"
+        width={width}
+        height={height}
         onMouseMove={onMouseMove}
-        viewBox={`0 0 ${width} ${height}`}
         preserveAspectRatio="xMinYMid"
         ref={svg}
       >
