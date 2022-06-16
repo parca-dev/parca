408b2b4d173af48b7e1ee2886fdc3b3534a43001
Maxime Brunet <max@brnt.mx>
2022-06-16 11:52:48 -0700

    build: fix silent container build failure and lint

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,24 @@
-ARG GOLANG_BASE
-ARG RUNNER_BASE
-
-FROM ${GOLANG_BASE} as builder
+FROM docker.io/golang:1.18.2-alpine3.15@sha256:e6b729ae22a2f7b6afcc237f7b9da3a27151ecbdcd109f7ab63a42e52e750262 AS builder
 RUN mkdir /.cache && chown nobody:nogroup /.cache && touch -t 202101010000.00 /.cache
+
+ARG GRPC_HEALTH_PROBE_VERSION=v0.4.11
 
 WORKDIR /app
 
-RUN go install github.com/grpc-ecosystem/grpc-health-probe@latest
+RUN go install "github.com/grpc-ecosystem/grpc-health-probe@${GRPC_HEALTH_PROBE_VERSION}"
 # Predicatable path for copying over to final image
-RUN if [ "$(go env GOHOSTARCH)" != "$(go env GOARCH)" ]; then mv "$(go env GOPATH)/bin/$(go env GOOS)_$(go env GOARCH)/grpc-health-probe" "$(go env GOPATH)/bin/grpc-health-probe"; fi
+RUN if [ "$(go env GOHOSTARCH)" != "$(go env GOARCH)" ]; then \
+        mv "$(go env GOPATH)/bin/$(go env GOOS)_$(go env GOARCH)/grpc-health-probe" "$(go env GOPATH)/bin/grpc-health-probe"; \
+    fi
 
 COPY ./dist /app/dist
-RUN if [ "amd64" = "$(go env GOARCH)" ]; then cp "dist/parca_$(go env GOOS)_$(go env GOARCH)_$(go env GOAMD64)/parca" parca; else cp "dist/parca_$(go env GOOS)_$(go env GOARCH)/parca" parca; fi
+RUN if [ "amd64" = "$(go env GOARCH)" ]; then \
+        cp "dist/parca_$(go env GOOS)_$(go env GOARCH)_$(go env GOAMD64)/parca" parca; \
+    else \
+        cp "dist/parca_$(go env GOOS)_$(go env GOARCH)/parca" parca; \
+    fi
 
-FROM ${RUNNER_BASE} as runner
+FROM docker.io/alpine:3.15.4@sha256:4edbd2beb5f78b1014028f4fbb99f3237d9561100b6881aabbf5acce2c4f9454 AS runner
 
 USER nobody
 
--- scripts/make-containers.sh
+++ scripts/make-containers.sh
@@ -1,34 +1,13 @@
 #!/usr/bin/env bash
-set +eox pipefail
+set -euo pipefail
 
-MANIFEST="$1"
-ARCHS=('amd64' 'arm64')
+MANIFEST="${1?Image name must be provided}"
+PLATFORMS=('linux/amd64' 'linux/arm64')
 
-# SHA order is respectively ('amd64' 'arm64')
-# this image is what docker.io/golang:1.18.2-alpine3.15 on May 12 2022
-DOCKER_GOLANG_ALPINE_SHAS=(
-    'docker.io/golang@sha256:3765360960a954c24b215518a41b0bf8e9e2fe3bd142b6f782fa56f8e00b8d21'
-    'docker.io/golang@sha256:394c1e739574aac2b389c628d0a199316164c3ed9d821c06ebe4ad3f133ed1f9'
-)
-
-# SHA order is respectively ('amd64' 'arm64')
-# this image is what docker.io/alpine:3.15.4 on May 11 2022.
-# Here is how to obtain the digests:
-# for r in amd64 arm64v8; do docker pull $r/alpine:3.15.4 | grep Digest; done
-DOCKER_ALPINE_SHAS=(
-    'docker.io/alpine@sha256:a777c9c66ba177ccfea23f2a216ff6721e78a662cd17019488c417135299cd89'
-    'docker.io/alpine@sha256:f3bec467166fd0e38f83ff32fb82447f5e89b5abd13264a04454c75e11f1cdc6'
-)
-
-for i in "${!ARCHS[@]}"; do
-    ARCH=${ARCHS[$i]}
-    DOCKER_GOLANG_ALPINE_SHA=${DOCKER_GOLANG_ALPINE_SHAS[$i]}
-    DOCKER_ALPINE_SHA=${DOCKER_ALPINE_SHAS[$i]}
-    echo "Building manifest for $MANIFEST with arch \"$ARCH\""
+for platform in "${PLATFORMS[@]}"; do
+    printf 'Building manifest for %s with platform "%s"\n' "${MANIFEST}" "${platform}"
     podman build \
-        --build-arg GOLANG_BASE="$DOCKER_GOLANG_ALPINE_SHA" \
-        --build-arg RUNNER_BASE="$DOCKER_ALPINE_SHA" \
-        --arch "$ARCH" \
+        --platform "${platform}" \
         --timestamp 0 \
-        --manifest "$MANIFEST" .; \
+        --manifest "${MANIFEST}" .
 done
