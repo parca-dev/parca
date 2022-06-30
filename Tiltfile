7b9a37490738aa37b9635e6e6f141eedd80c05eb
Kemal Akkoyun <kakkoyun@users.noreply.github.com>
2022-06-30 10:53:33 +0200

    Improve local dev setup (#1274)
    Signed-off-by: Kemal Akkoyun <kakkoyun@gmail.com>

--- Tiltfile
+++ Tiltfile
@@ -2,31 +2,42 @@ docker_prune_settings(num_builds=5)
 
 # Parca
 
-## API Only
-docker_build(
-    'parca.io/parca/parca:dev', '.',
-    dockerfile='Dockerfile.go.dev',
-    only=['./cmd', './pkg', './proto', './gen', './go.mod', './go.sum', 'parca.yaml'],
-)
-
 ## All-in-one
-# docker_build('parca.io/parca/parca:dev', '.',
-#     dockerfile='Dockerfile.dev',
-#     only=['./cmd', './pkg', './proto', './gen', './ui', './go.mod', './go.sum', 'parca.yaml'],
-# )
+docker_build(
+    'localhost:5000/parca:dev', '',
+    dockerfile='Dockerfile.dev',
+    only=[
+        './cmd',
+        './pkg',
+        './proto',
+        './gen',
+        './ui',
+        './go.mod',
+        './go.sum',
+        'parca.yaml',
+    ],
+)
 
 k8s_yaml('deploy/tilt/parca-server-deployment.yaml')
 k8s_resource('parca', port_forwards=[7070])
 
-## UI
-docker_build(
-    'parca.io/parca/parca-ui:dev', './ui',
-    entrypoint='yarn workspace @parca/web dev',
-    dockerfile='./ui/Dockerfile.dev',
-    live_update=[
-        sync('./ui', '/app'),
-        run('cd /app && yarn install', trigger=['./package.json', './yarn.lock']),
-    ],
-)
-k8s_yaml('deploy/tilt/parca-ui-deployment.yaml')
-k8s_resource('parca-ui', port_forwards=3000)
+## API Only
+# docker_build(
+#     'localhost:5000.parca:dev', '.',
+#     dockerfile='Dockerfile.go.dev',
+#     only=['./cmd', './pkg', './proto', './gen', './go.mod', './go.sum', 'parca.yaml'],
+# )
+
+# TODO(kakkoyun): Make conditional.
+## UI Only
+# docker_build(
+#     'localhost:5000/parca-ui:dev', './ui',
+#     entrypoint='yarn workspace @parca/web dev',
+#     dockerfile='./ui/Dockerfile.dev',
+#     live_update=[
+#         sync('./ui', '/app'),
+#         run('cd /app && yarn install', trigger=['./package.json', './yarn.lock']),
+#     ],
+# )
+# k8s_yaml('deploy/tilt/parca-ui-deployment.yaml')
+# k8s_resource('parca-ui', port_forwards=3000)
--- scripts/local-dev.sh
+++ scripts/local-dev.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+# Copyright (c) 2022 The Parca Authors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 ################################################################################
 #
 # This script is meant to be run from the root of this project with the Makefile
@@ -8,32 +22,82 @@
 
 set -euo pipefail
 
+NODE_COUNT=${NODE_COUNT:-1}
+
+MINIKUBE_PROFILE_NAME="${MINIKUBE_PROFILE_NAME:-parca}"
+function mk() {
+    minikube -p "${MINIKUBE_PROFILE_NAME}" "$@"
+}
+
 # Creates a local minikube cluster, and deploys the dev env into the cluster
 function up() {
     # Spin up local cluster if one isn't running
-    if minikube status -p parca; then
+    if mk status; then
         echo "----------------------------------------------------------"
-        echo "Dev cluster already running. Skipping minikube cluster creation"
+        echo "Dev cluster already running"
+        echo "Skipping minikube cluster creation"
         echo "----------------------------------------------------------"
     else
-        ctlptl create registry ctlptl-registry || echo 'Registry already exists'
-        minikube start -p parca --driver=virtualbox --kubernetes-version=v1.22.3 --cpus=4 --disk-size=80000mb --docker-opt dns=8.8.8.8
-        # minikube start -p parca --driver=kvm2 --kubernetes-version=v1.22.3 --cpus=12 --memory=40gb --disk-size=120gb --docker-opt dns=8.8.8.8
+        # local_registry
+
+        echo "----------------------------------------------------------"
+        echo "Creating minikube cluster"
+        echo "----------------------------------------------------------"
+        # kvm2, hyperkit, hyperv, vmwarefu1sion, virtualbox, vmware, xhyve
+        mk start \
+            --driver=kvm2 \
+            --nodes=${NODE_COUNT} \
+            --kubernetes-version=v1.23.3 \
+            --cpus=4 \
+            --memory=8gb \
+            --disk-size=80000mb \
+            --docker-opt dns=8.8.8.8 \
+            --docker-opt default-ulimit=memlock=9223372036854775807:9223372036854775807
     fi
+
+    trap 'kill $(jobs -p)' SIGINT SIGTERM EXIT
+
+    # Configure registry in minikube
+    minikube_registry
 
     # Deploy all services into the cluster
     deploy
 
-    echo "Now run \"tilt up\" to start developing!"
+    # Start the Tilt
+    tilt up
 }
 
 # Tears down a local minikube cluster
 function down() {
-    minikube delete -p parca
+    mk delete
 }
 
 # Deploys the dev env into the minikube cluster
 function deploy() {
+    echo "----------------------------------------------------------"
+    echo "Deploying dev environment"
+    echo "----------------------------------------------------------"
     # Deploy all generated manifests
     kubectl apply -R -f ./deploy/tilt
+}
+
+function minikube_registry() {
+    mk addons enable registry
+    kubectl port-forward -n kube-system svc/registry 5000:80 &
+}
+
+reg_name='minikube-registry'
+reg_port='5000'
+
+# Configures a registry using localhost docker runtime.
+function local_registry() {
+    echo "----------------------------------------------------------"
+    echo "Checking if registry exists/Creating registry"
+    echo "----------------------------------------------------------"
+    running="$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)"
+    if [ "${running}" != 'true' ]; then
+        docker run \
+         -d --restart=always -p "${reg_port}:5000" --name "${reg_name}" \
+            registry:2
+    fi
 }
