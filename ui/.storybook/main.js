550122e900c0f9abd51e1fac3e23f2c8dc7c285c
Yomi Eluwande <yomi.eluwande@gmail.com>
2022-09-20 12:30:34 +0100

    ui: Setup storybook in the `ui` folder

--- ui/.storybook/main.js
+++ ui/.storybook/main.js
@@ -4,17 +4,43 @@ module.exports = {
   reactOptions: {
     strictMode: true,
   },
+  features: {
+    storyStoreV7: true,
+  },
+  framework: '@storybook/react',
   core: {
     builder: 'webpack5',
   },
   stories: ['../packages/**/*.stories.mdx', '../packages/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-docs',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-dark-mode',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+          postcssOptions: {
+            path: '../packages/app/web/postcss.config.js',
+          },
+        },
+      },
+    },
+  ],
+  // A workaround for a storybook regression, see https://github.com/storybookjs/storybook/issues/14197#issuecomment-949337652
+  babel: async options => ({
+    ...options,
+    plugins: [['@babel/plugin-proposal-class-properties', {loose: true}]],
+  }),
   webpackFinal: async (config, {configType}) => {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
     // You can change the configuration based on that.
     // 'PRODUCTION' is used when building the static version of storybook.
 
     // Make whatever fine-grained changes you need
+    config.module.rules[0].test = /\.(mjs|tsx?|ts?|jsx?)$/;
     config.module.rules.push({
       test: /\.scss$/,
       use: [
@@ -22,6 +48,14 @@ module.exports = {
         {
           loader: 'css-loader',
           options: {modules: true},
+        },
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: ['tailwindcss', 'autoprefixer'],
+            },
+          },
         },
         {loader: 'sass-loader'},
       ],
--- ui/.storybook/preview.js
+++ ui/.storybook/preview.js
@@ -1,3 +1,15 @@
-module.exports.parameters = {
+// import 'tailwindcss/tailwind.css';
+import './sb-tailwind.css';
+// import '../packages/app/web/src/style/file-input.css';
+// import '../packages/app/web/src/style/metrics.css';
+// import '../packages/app/web/src/style/profile.css';
+// import '../packages/app/web/src/style/sidenav.css';
+
+export const parameters = {
   actions: {argTypesRegex: '^on[A-Z].*'},
+  darkMode: {
+    darkClass: 'dark',
+    lightClass: 'light',
+    stylePreview: true,
+  },
 };
