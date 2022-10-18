613cfc3de5a88810a7f11e097c69c2ef131d3ef1
Yomi Eluwande <yomi.eluwande@gmail.com>
2022-10-18 13:24:10 +0200

    ui: Truncate buildId text

--- ui/packages/shared/profile/src/utils.ts
+++ ui/packages/shared/profile/src/utils.ts
@@ -42,3 +42,11 @@ export const downloadPprof = async (
   const blob = new Blob([response.report.pprof], {type: 'application/octet-stream'});
   return blob;
 };
+
+export const truncateString = (str: string, num: number) => {
+  if (str.length <= num) {
+    return str;
+  }
+
+  return str.slice(0, num) + '...';
+};
