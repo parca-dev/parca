79993978588f92c13b9f23f9ec813517ccb53169
Manoj Vivek <p.manoj.vivek@gmail.com>
2023-01-03 17:14:27 +0530

    ui: Github Star button and Release Notes Viewer which show the release notes within Parca UI (#2366)
    * Added Release Notes Viewer which automatically opens when new release is deployed or when the version number is clicked
    
    * Added what's new to the version text

---
+++ ui/packages/app/web/src/components/ReleaseNotesViewer/index.tsx
@@ -0,0 +1,115 @@
+// Copyright 2022 The Parca Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {useEffect, useState} from 'react';
+import {useCookie} from 'react-use';
+import cx from 'classnames';
+import ReactMarkdown from 'react-markdown';
+import {Modal, Button} from '@parca/components';
+
+interface Props {
+  version: string;
+}
+
+const ReleaseNotesViewer = ({version}: Props) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isReleaseNotesAvailable, setIsReleaseNotesAvailable] = useState<boolean>(false);
+  const [releaseNotes, setReleaseNotes] = useState<string>('');
+  const [viewedRelease, setViewedRelease] = useCookie('viewedReleaseNotes');
+
+  useEffect(() => {
+    (async () => {
+      if (version === '{{.Version}}') {
+        setIsReleaseNotesAvailable(false);
+        return;
+      }
+      const release = await fetch(
+        `https://api.github.com/repos/parca-dev/parca/releases/tags/${version}`
+      ).then(res => res.json());
+      setReleaseNotes(
+        `Here's the list of changes in this release:\n${release.body
+          .replaceAll(/(https:\/\/github.com\/([^\s]*))/g, `[$2]($1)`)
+          .replaceAll(/\*\*Full.*/g, ``)
+          .replaceAll(/@(\w+)/g, '[@$1](https://github.com/$1)')
+          .replaceAll(/##/g, `###`) // Convert headers to one level lower
+          .trim()}`
+      );
+
+      setIsReleaseNotesAvailable(true);
+      if (viewedRelease !== version) {
+        setIsOpen(true);
+      }
+    })();
+  }, [version, viewedRelease]);
+
+  const onClose = () => {
+    setIsOpen(false);
+    if (viewedRelease !== version) {
+      setViewedRelease(version, {path: '/', expires: 180});
+    }
+  };
+
+  return (
+    <>
+      <span
+        onClick={() => {
+          setIsOpen(true);
+        }}
+        className={cx({'cursor-pointer': isReleaseNotesAvailable})}
+      >
+        {version}
+        {isReleaseNotesAvailable ? ` - What's new?` : ''}
+      </span>
+      {isReleaseNotesAvailable ? (
+        <Modal
+          className="w-3/5 h-[80vh]"
+          isOpen={isOpen}
+          closeModal={onClose}
+          title={`What's new in Parca ${version} 🎉`}
+        >
+          <div className="flex flex-col h-full pb-4 text-gray-800 dark:text-gray-200">
+            <div className="prose dark:prose-invert overflow-scroll max-w-none pt-2 flex-1 prose-gray">
+              <ReactMarkdown
+                components={{
+                  // eslint-disable-next-line jsx-a11y/anchor-has-content
+                  a: ({node, ...props}) => <a target="_blank" rel="noreferrer" {...props} />,
+                }}
+              >
+                {releaseNotes}
+              </ReactMarkdown>
+            </div>
+            <div className="flex gap-2 justify-between items-center mt-4">
+              <Button variant="neutral" className="w-fit" onClick={onClose}>
+                Close
+              </Button>
+              <Button
+                className="w-fit"
+                onClick={() => {
+                  window.open(
+                    `https://github.com/parca-dev/parca/releases/tag/${version}`,
+                    '_blank'
+                  );
+                  onClose();
+                }}
+              >
+                Full Changelog
+              </Button>
+            </div>
+          </div>
+        </Modal>
+      ) : null}
+    </>
+  );
+};
+
+export default ReleaseNotesViewer;
