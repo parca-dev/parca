8766334deed104538c2925766bae356f27ebad62
Manoj Vivek <p.manoj.vivek@gmail.com>
2023-02-09 16:44:20 +0530

    Added a screen with message when no data is pushed to the server

---
+++ ui/packages/shared/components/src/NoDataPrompt/index.tsx
@@ -0,0 +1,60 @@
+// Copyright 2022 The Parca Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {Icon} from '@iconify/react';
+
+declare global {
+  interface Window {
+    PATH_PREFIX: string;
+  }
+}
+
+const pathPrefix = process.env.NODE_ENV === 'development' ? '' : window.PATH_PREFIX;
+
+export const NoDataPrompt = (): JSX.Element => {
+  return (
+    <div className="flex flex-col items-center h-96 justify-center gap-6">
+      <Icon icon="material-symbols:info-outline" width={48} height={48} />
+      <p className="text-center">
+        The Parca server hasn&apos;t recieved any data yet! <br />
+        <br />
+        Please check the{' '}
+        <a href={`${pathPrefix}/targets`} className="text-blue-500">
+          targets
+        </a>{' '}
+        page and ensure that the agents are configured correctly and sending data to the server.
+        <br />
+        <br />
+        If you&apos;re still having trouble, please check out the{' '}
+        <a
+          href="https://www.parca.dev/docs/"
+          className="text-blue-500"
+          target="_blank"
+          rel="noreferrer"
+        >
+          documentation
+        </a>{' '}
+        or join our{' '}
+        <a
+          href="https://discord.gg/ZgUpYgpzXy"
+          className="text-blue-500"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Discord
+        </a>{' '}
+        for help.
+      </p>
+    </div>
+  );
+};
