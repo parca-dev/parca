24736bf253a3921838d79d82c762a082e6b3a2ec
Manoj Vivek <p.manoj.vivek@gmail.com>
2023-03-14 15:13:03 +0530

    Storybook setup with v7

--- ui/.storybook/preview.js
+++ ui/.storybook/preview.js
@@ -1,3 +1,12 @@
-module.exports.parameters = {
+import '../packages/shared/components/dist/styles.css';
+import '../packages/shared/profile/dist/styles.css';
+
+export const parameters = {
   actions: {argTypesRegex: '^on[A-Z].*'},
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
 };
