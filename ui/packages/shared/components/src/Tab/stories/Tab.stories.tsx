7e5199e86041d6832a9ef3c8239c426ffd151b19
monicawoj <monica.wojciechowski@polarsignals.com>
2023-03-28 10:45:51 +0200

    move story files to stories subfolder

--- ui/packages/shared/components/src/Tab/stories/Tab.stories.tsx
+++ ui/packages/shared/components/src/Tab/stories/Tab.stories.tsx
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import Tab from './';
+import Tab from '..';
 
 export default {
   component: Tab,
