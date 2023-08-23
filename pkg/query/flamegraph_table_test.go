2c37ffab3ccaaf673270302eb5d30c09fcebeb97
thorfour <me@thor-hansen.com>
2023-08-23 16:42:34 -0500

    TestFlamegraphTrimmingAndFiltering: fix memory leaks

--- pkg/query/flamegraph_table_test.go
+++ pkg/query/flamegraph_table_test.go
@@ -1226,6 +1226,11 @@ func TestFlamegraphTrimmingAndFiltering(t *testing.T) {
 	defer mem.AssertSize(t, 0)
 	newProfile.Samples, filtered, err = FilterProfileData(ctx, tracer, mem, newProfile.Samples, "b") // querying for "b" should filter out the "5.c" function.
 	require.NoError(t, err)
+	defer func() {
+		for _, s := range newProfile.Samples {
+			s.Release()
+		}
+	}()
 
 	p, err = parcacol.NewArrowToProfileConverter(tracer, metastore.NewKeyMaker()).Convert(ctx, newProfile)
 	require.NoError(t, err)
