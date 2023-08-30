parca-dev/parca

commit 70bebf4d5f611432c76c5eb36d67b92cdb7b439d

ui: Minor UI fixes (#3714)
* Fix to show the loader on top of the source pane

* Line number yellow bg fills the entire line's width

* Flamegraph tooltip reference boundary modified to be the viewport

* Flamegraph error is not blocking the entire visualization panel

ui/packages/shared/profile/src/ProfileIcicleGraph/index.tsx
@@ -47,6 +47,7 @@ interface ProfileIcicleGraphProps {
   navigateTo?: NavigateFunction;
   loading: boolean;
   setActionButtons?: (buttons: React.JSX.Element) => void;
+  error?: any;
 }
 
 const GroupAndSortActionButtons = ({navigateTo}: {navigateTo?: NavigateFunction}): JSX.Element => {
@@ -111,6 +112,7 @@ const ProfileIcicleGraph = function ProfileIcicleGraphNonMemo({
   navigateTo,
   loading,
   setActionButtons,
+  error,
 }: ProfileIcicleGraphProps): JSX.Element {
   const compareMode: boolean =
     selectQueryParam('compare_a') === 'true' && selectQueryParam('compare_b') === 'true';
@@ -174,6 +176,11 @@ const ProfileIcicleGraph = function ProfileIcicleGraphNonMemo({
       </div>
     );
   }, [navigateTo, table, curPath, setNewCurPath, setActionButtons]);
+
+  if (error != null) {
+    console.error('Error: ', error);
+    return <div className="flex justify-center p-10">An error occurred: {error.message}</div>;
+  }
 
   if (graph === undefined && table === undefined) return <div>no data...</div>;
 

ui/packages/shared/profile/src/ProfileView/index.tsx
@@ -96,7 +96,7 @@ interface SourceData {
 export interface ProfileViewProps {
   total: bigint;
   filtered: bigint;
-  flamegraphData?: FlamegraphData;
+  flamegraphData: FlamegraphData;
   topTableData?: TopTableData;
   callgraphData?: CallgraphData;
   sourceData?: SourceData;
@@ -229,15 +229,6 @@ export const ProfileView = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [graphvizLoaded, callgraphData?.data]);
 
-  if (flamegraphData?.error !== null) {
-    console.error('Error: ', flamegraphData?.error);
-    return (
-      <div className="flex justify-center p-10">
-        An error occurred: {flamegraphData?.error.message}
-      </div>
-    );
-  }
-
   const setNewCurPath = (path: string[]): void => {
     if (!arrayEquals(curPath, path)) {
       setCurPath(path);
@@ -255,7 +246,7 @@ export const ProfileView = ({
   }): JSX.Element => {
     switch (type) {
       case 'icicle': {
-        return flamegraphData?.table !== undefined || flamegraphData.data !== undefined ? (
+        return (
           <ConditionalWrapper<ProfilerProps>
             condition={perf?.onRender != null}
             WrapperComponent={Profiler}
@@ -267,18 +258,17 @@ export const ProfileView = ({
             <ProfileIcicleGraph
               curPath={curPath}
               setNewCurPath={setNewCurPath}
-              table={flamegraphData.table}
-              graph={flamegraphData.data}
+              table={flamegraphData?.table}
+              graph={flamegraphData?.data}
               total={total}
               filtered={filtered}
               sampleUnit={sampleUnit}
               navigateTo={navigateTo}
               loading={flamegraphData.loading}
               setActionButtons={setActionButtons}
+              error={flamegraphData.error}
             />
           </ConditionalWrapper>
-        ) : (
-          <> </>
         );
       }
       case 'callgraph': {

ui/packages/shared/profile/src/GraphTooltipArrow/index.tsx
@@ -76,19 +76,12 @@ const GraphTooltip = ({
           options: {
             tether: false,
             altAxis: true,
-            boundary: contextElement ?? undefined,
           },
         },
         {
           name: 'offset',
           options: {
             offset: [30, 30],
-          },
-        },
-        {
-          name: 'flip',
-          options: {
-            boundary: contextElement ?? undefined,
           },
         },
       ],
@@ -129,7 +122,7 @@ const GraphTooltip = ({
   return isFixed ? (
     <>{children}</>
   ) : (
-    <div ref={setPopperElement} style={styles.popper} {...attributes.popper}>
+    <div ref={setPopperElement} style={styles.popper} {...attributes.popper} className="z-10">
       {children}
     </div>
   );

ui/packages/shared/profile/src/SourceView/Highlighter.tsx
@@ -147,7 +147,7 @@ export const profileAwareRenderer = (
               <LineProfileMetadata value={flat?.get(i) ?? 0n} total={total} filtered={filtered} />
               <div
                 className={cx(
-                  'w-11/12 flex-grow-0 border-l border-gray-200 pl-1 dark:border-gray-700',
+                  'w-full flex-grow-0 border-l border-gray-200 pl-1 dark:border-gray-700',
                   {
                     'bg-yellow-200 dark:bg-yellow-700': isCurrentLine,
                   }

commit 499235345dc15753933a2d50e8b3e5a709090664

ui: Revert regression from #3714

ui/packages/shared/profile/src/ProfileIcicleGraph/index.tsx
@@ -47,7 +47,6 @@ interface ProfileIcicleGraphProps {
   navigateTo?: NavigateFunction;
   loading: boolean;
   setActionButtons?: (buttons: React.JSX.Element) => void;
-  error?: any;
 }
 
 const GroupAndSortActionButtons = ({navigateTo}: {navigateTo?: NavigateFunction}): JSX.Element => {
@@ -112,7 +111,6 @@ const ProfileIcicleGraph = function ProfileIcicleGraphNonMemo({
   navigateTo,
   loading,
   setActionButtons,
-  error,
 }: ProfileIcicleGraphProps): JSX.Element {
   const compareMode: boolean =
     selectQueryParam('compare_a') === 'true' && selectQueryParam('compare_b') === 'true';
@@ -176,11 +174,6 @@ const ProfileIcicleGraph = function ProfileIcicleGraphNonMemo({
       </div>
     );
   }, [navigateTo, table, curPath, setNewCurPath, setActionButtons]);
-
-  if (error != null) {
-    console.error('Error: ', error);
-    return <div className="flex justify-center p-10">An error occurred: {error.message}</div>;
-  }
 
   if (graph === undefined && table === undefined)
     return <div className="mx-auto text-center">no data...</div>;

ui/packages/shared/profile/src/ProfileView/index.tsx
@@ -96,7 +96,7 @@ interface SourceData {
 export interface ProfileViewProps {
   total: bigint;
   filtered: bigint;
-  flamegraphData: FlamegraphData;
+  flamegraphData?: FlamegraphData;
   topTableData?: TopTableData;
   callgraphData?: CallgraphData;
   sourceData?: SourceData;
@@ -229,6 +229,15 @@ export const ProfileView = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [graphvizLoaded, callgraphData?.data]);
 
+  if (flamegraphData?.error !== null) {
+    console.error('Error: ', flamegraphData?.error);
+    return (
+      <div className="flex justify-center p-10">
+        An error occurred: {flamegraphData?.error.message}
+      </div>
+    );
+  }
+
   const setNewCurPath = (path: string[]): void => {
     if (!arrayEquals(curPath, path)) {
       setCurPath(path);
@@ -246,7 +255,7 @@ export const ProfileView = ({
   }): JSX.Element => {
     switch (type) {
       case 'icicle': {
-        return (
+        return flamegraphData?.table !== undefined || flamegraphData.data !== undefined ? (
           <ConditionalWrapper<ProfilerProps>
             condition={perf?.onRender != null}
             WrapperComponent={Profiler}
@@ -258,8 +267,8 @@ export const ProfileView = ({
             <ProfileIcicleGraph
               curPath={curPath}
               setNewCurPath={setNewCurPath}
-              table={flamegraphData?.table}
-              graph={flamegraphData?.data}
+              table={flamegraphData.table}
+              graph={flamegraphData.data}
               total={total}
               filtered={filtered}
               sampleUnit={sampleUnit}
@@ -269,6 +278,8 @@ export const ProfileView = ({
               error={flamegraphData.error}
             />
           </ConditionalWrapper>
+        ) : (
+          <></>
         );
       }
       case 'callgraph': {