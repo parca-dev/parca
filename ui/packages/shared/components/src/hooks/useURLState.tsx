34a34129bf3055febfd8d30f8faea857b4495b88
Manoj Vivek <p.manoj.vivek@gmail.com>
2023-11-01 16:43:21 +0530

    Fixing useURLState to be SSR proof (#4027)

--- ui/packages/shared/components/src/hooks/useURLState.tsx
+++ ui/packages/shared/components/src/hooks/useURLState.tsx
@@ -50,7 +50,7 @@ export const useURLState = ({
   withURLUpdate = true,
 }: Props): [string | string[], (val: string | string[]) => void] => {
   const dispatch = useAppDispatch();
-  const router = parseParams(window?.location.search);
+  const router = typeof window !== 'undefined' ? parseParams(window?.location.search) : {};
   const [highlightAfterFilteringEnabled] = useUserPreference<boolean>(
     USER_PREFERENCES.HIGHTLIGHT_AFTER_FILTERING.key
   );
