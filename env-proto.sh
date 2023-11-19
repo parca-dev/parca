9b9f659b46a281030a97bc5ef7004b7831f7fc05
Maxime Brunet <max@brnt.mx>
2023-11-18 17:32:07 -0800

    Generate

--- env-proto.sh
+++ env-proto.sh
@@ -14,7 +14,8 @@
 
 set -euo pipefail
 
-BIN_DIR=${BIN_DIR:-/usr/local/bin}
+BIN_DIR="$(go env GOBIN)"
+BIN_DIR="${BIN_DIR:-$(go env GOPATH)/bin}"
 
 # renovate: datasource=github-releases depName=bufbuild/buf
 BUF_VERSION='v1.28.0'
