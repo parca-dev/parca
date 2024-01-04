79fbfc91a5b0fef34db90a379fd7be972ba91384
Manoj Vivek <p.manoj.vivek@gmail.com>
2024-01-04 15:03:14 +0530

    ESLint fix

--- ui/packages/shared/components/src/DateTimeRangePicker/utils.ts
+++ ui/packages/shared/components/src/DateTimeRangePicker/utils.ts
@@ -11,7 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import format from 'date-fns/format';
 import moment from 'moment-timezone';
 
 import {ABSOLUTE_TIME_ALIASES, AbsoluteDateValue, DATE_FORMAT} from '../DateTimePicker';
