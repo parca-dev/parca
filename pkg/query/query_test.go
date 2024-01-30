960147c3ac0da18de49828bff51a2809666e4707
Matthias Loibl <mail@matthiasloibl.com>
2024-01-30 17:59:10 +0100

    pkg/query: Benchmark merges using delta cpu profiles

--- pkg/query/query_test.go
+++ pkg/query/query_test.go
@@ -68,7 +68,7 @@ func Benchmark_Query_Merge(b *testing.B) {
 				tracer,
 			))
 
-			fileContent, err := os.ReadFile("../query/testdata/alloc_objects.pb.gz")
+			fileContent, err := os.ReadFile("../query/testdata/profile1.pb.gz")
 			require.NoError(b, err)
 
 			store := profilestore.NewProfileColumnStore(
@@ -91,7 +91,7 @@ func Benchmark_Query_Merge(b *testing.B) {
 							Labels: []*profilestorepb.Label{
 								{
 									Name:  "__name__",
-									Value: "memory",
+									Value: "cpu",
 								},
 								{
 									Name:  "job",
@@ -133,11 +133,11 @@ func Benchmark_Query_Merge(b *testing.B) {
 			b.ResetTimer()
 
 			for i := 0; i < b.N; i++ {
-				_, err = api.Query(ctx, &pb.QueryRequest{
+				resp, err := api.Query(ctx, &pb.QueryRequest{
 					Mode: pb.QueryRequest_MODE_MERGE,
 					Options: &pb.QueryRequest_Merge{
 						Merge: &pb.MergeProfile{
-							Query: `{__name__="memory:alloc_objects:count:space:bytes"}`,
+							Query: `{__name__="cpu:samples:count:cpu:nanoseconds:delta"}`,
 							Start: timestamppb.New(time.Unix(0, math.MinInt64)),
 							End:   timestamppb.New(time.Unix(0, math.MaxInt64)),
 						},
@@ -145,6 +145,7 @@ func Benchmark_Query_Merge(b *testing.B) {
 					//nolint:staticcheck // SA1019: Fow now we want to support these APIs
 					ReportType: pb.QueryRequest_REPORT_TYPE_FLAMEGRAPH_ARROW,
 				})
+				require.NotEqual(b, int64(0), resp.Total)
 				require.NoError(b, err)
 			}
 		})
