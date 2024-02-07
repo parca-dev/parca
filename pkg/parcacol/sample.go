04a59f2aa0251be18ad7fffffef32dc54769c3c6
Frederic Branczyk <fbranczyk@gmail.com>
2024-02-07 12:15:15 +0100

    Build arrow records without converting to parquet for sorting

--- pkg/parcacol/sample.go
+++ pkg/parcacol/sample.go
@@ -16,15 +16,16 @@ package parcacol
 import (
 	"context"
 	"fmt"
-	"io"
 	"strings"
 
 	"github.com/apache/arrow/go/v14/arrow"
 	"github.com/apache/arrow/go/v14/arrow/array"
+	"github.com/apache/arrow/go/v14/arrow/compute"
 	"github.com/apache/arrow/go/v14/arrow/memory"
 	"github.com/parquet-go/parquet-go"
 	"github.com/polarsignals/frostdb/dynparquet"
 	"github.com/polarsignals/frostdb/pqarrow"
+	"github.com/polarsignals/frostdb/pqarrow/arrowutils"
 	"github.com/polarsignals/frostdb/query/logicalplan"
 
 	"github.com/parca-dev/parca/pkg/normalizer"
@@ -224,100 +225,46 @@ func SeriesToArrowRecord(
 		}
 	}
 
-	return bldr.NewRecord(), nil
-}
-
-// ParquetBufToArrowRecord converts a parquet buffer to an arrow record. If rowsPerRecord is 0, then the entire buffer is converted to a single record.
-func ParquetBufToArrowRecord(ctx context.Context, mem memory.Allocator, buf *dynparquet.Buffer, s *dynparquet.Schema, rowsPerRecord uint) ([]arrow.Record, error) {
-	as, err := pqarrow.ParquetSchemaToArrowSchema(ctx, buf.Schema(), s, logicalplan.IterOptions{})
-	if err != nil {
-		return nil, err
-	}
-
-	bldr := array.NewRecordBuilder(mem, as)
-	defer bldr.Release()
-
-	rows := buf.Rows()
-	defer rows.Close()
-
-	buffSize := 256
-	rowBuf := make([]parquet.Row, buffSize)
-
-	records := []arrow.Record{}
-	recordSize := uint(0)
-	for {
-		n, err := rows.ReadRows(rowBuf)
-		if err != nil && err != io.EOF {
-			return nil, err
+	sortingColumns := []arrowutils.SortingColumn{}
+	arrowFields := as.Fields()
+	for _, col := range schema.SortingColumns() {
+		direction := arrowutils.Ascending
+		if col.Descending() {
+			direction = arrowutils.Descending
 		}
 
-		if n == 0 {
-			break
+		colDef, found := schema.ColumnByName(col.ColumnName())
+		if !found {
+			return nil, fmt.Errorf("sorting column %v not found in schema", col.ColumnName())
 		}
 
-		// Write each row to the arrow record
-		for _, r := range rowBuf[:n] {
-			for i := range r {
-				switch as.Field(i).Name {
-				case profile.ColumnName:
-					fallthrough
-				case profile.ColumnPeriodType:
-					fallthrough
-				case profile.ColumnPeriodUnit:
-					fallthrough
-				case profile.ColumnSampleType:
-					fallthrough
-				case profile.ColumnSampleUnit:
-					if err := bldr.Field(i).(*array.BinaryDictionaryBuilder).AppendString(r[i].String()); err != nil {
-						return nil, err
-					}
-				case profile.ColumnStacktrace:
-					bldr.Field(i).(*array.BinaryBuilder).AppendString(r[i].String())
-				case profile.ColumnDuration:
-					fallthrough
-				case profile.ColumnPeriod:
-					fallthrough
-				case profile.ColumnTimestamp:
-					fallthrough
-				case profile.ColumnValue:
-					bldr.Field(i).(*array.Int64Builder).Append(r[i].Int64())
-				default:
-					switch {
-					case strings.HasPrefix(as.Field(i).Name, profile.ColumnPprofNumLabels):
-						if r[i].IsNull() {
-							bldr.Field(i).AppendNull()
-						} else {
-							bldr.Field(i).(*array.Int64Builder).Append(r[i].Int64())
-						}
-					case strings.HasPrefix(as.Field(i).Name, profile.ColumnPprofLabels):
-						fallthrough
-					case strings.HasPrefix(as.Field(i).Name, profile.ColumnLabels):
-						if r[i].IsNull() {
-							bldr.Field(i).AppendNull()
-						} else {
-							if err := bldr.Field(i).(*array.BinaryDictionaryBuilder).AppendString(r[i].String()); err != nil {
-								return nil, err
-							}
-						}
-					default:
-						panic(fmt.Sprintf("unknown column %v", as.Field(i).Name))
-					}
+		if colDef.Dynamic {
+			for i, c := range arrowFields {
+				if strings.HasPrefix(c.Name, colDef.Name) {
+					sortingColumns = append(sortingColumns, arrowutils.SortingColumn{
+						Index:      i,
+						Direction:  direction,
+						NullsFirst: col.NullsFirst(),
+					})
 				}
 			}
-
-			recordSize++
-			if rowsPerRecord != 0 && recordSize >= rowsPerRecord {
-				records = append(records, bldr.NewRecord())
-				recordSize = 0
+		} else {
+			indices := as.FieldIndices(colDef.Name)
+			for _, i := range indices {
+				sortingColumns = append(sortingColumns, arrowutils.SortingColumn{
+					Index:      i,
+					Direction:  direction,
+					NullsFirst: col.NullsFirst(),
+				})
 			}
 		}
 	}
 
-	// Append final record
 	r := bldr.NewRecord()
-	if r.NumRows() > 0 {
-		records = append(records, r)
+	sortedIndexes, err := arrowutils.SortRecord(r, sortingColumns)
+	if err != nil {
+		return nil, fmt.Errorf("sort record: %w", err)
 	}
 
-	return records, nil
+	return arrowutils.Take(compute.WithAllocator(ctx, mem), r, sortedIndexes)
 }
