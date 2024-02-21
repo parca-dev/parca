f16b1572a4bddc19a21e93973499aeefd9ed6796
Manoj Vivek <p.manoj.vivek@gmail.com>
2024-02-21 10:18:49 +0530

    Removed an unwanted import

--- ui/packages/shared/icons/src/CloseIcon.tsx
+++ ui/packages/shared/icons/src/CloseIcon.tsx
@@ -11,8 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import React from 'react';
-
 const CloseIcon = (): JSX.Element => {
   return (
     <>
