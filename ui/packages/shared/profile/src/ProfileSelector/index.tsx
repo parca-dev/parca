80c6b3eefa9026ca1322a472a47bf0d8e79ccb8d
Manoj Vivek <p.manoj.vivek@gmail.com>
2024-04-02 16:20:18 +0530

    ui: Fixed the bug that was resetting the profile selection state on page load (#4476)
    * Fixed the bug that was resetting the profile selection state on page load
    
    * Linter fix

--- ui/packages/shared/profile/src/ProfileSelector/index.tsx
+++ ui/packages/shared/profile/src/ProfileSelector/index.tsx
@@ -99,7 +99,6 @@ const ProfileSelector = ({
     data: profileTypesData,
     error,
   } = useProfileTypes(queryClient);
-  const [isDataLoading, setIsDataLoading] = useState<boolean>(false);
   const {heightStyle} = useMetricsGraphDimensions(comparing);
   const {viewComponent} = useParcaContext();
 
@@ -108,18 +107,6 @@ const ProfileSelector = ({
   );
 
   const [queryExpressionString, setQueryExpressionString] = useState(querySelection.expression);
-
-  useEffect(() => {
-    if (querySelection.expression === undefined) {
-      return;
-    }
-
-    setIsDataLoading(true);
-    setQueryExpression();
-    setIsDataLoading(false);
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [timeRangeSelection]);
 
   useEffect(() => {
     if (enforcedProfileName !== '') {
@@ -296,8 +283,7 @@ const ProfileSelector = ({
       </div>
       <div className="rounded bg-white shadow dark:border-gray-500 dark:bg-gray-700">
         <div style={{height: heightStyle}}>
-          {!isDataLoading &&
-          querySelection.expression !== undefined &&
+          {querySelection.expression !== undefined &&
           querySelection.expression.length > 0 &&
           querySelection.from !== undefined &&
           querySelection.to !== undefined ? (
