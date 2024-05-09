40f3a369e551ecd84897f10eb79a74cfc22bc210
Manoj Vivek <p.manoj.vivek@gmail.com>
2024-05-09 16:06:20 +0530

    Added invert call stack option to Share Query Request

--- ui/packages/shared/client/src/google/api/http.ts
+++ ui/packages/shared/client/src/google/api/http.ts
@@ -2,7 +2,7 @@
 // @generated from protobuf file "google/api/http.proto" (package "google.api", syntax proto3)
 // tslint:disable
 //
-// Copyright 2023 Google LLC
+// Copyright 2024 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
