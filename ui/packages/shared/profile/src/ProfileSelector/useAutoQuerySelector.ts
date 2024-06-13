b913a2165178a73d07accd526817a0a0bd66a5c0
Frederic Branczyk <fbranczyk@gmail.com>
2024-06-13 14:52:26 +0200

    UI profile type pre-selection precedence

--- ui/packages/shared/profile/src/ProfileSelector/useAutoQuerySelector.ts
+++ ui/packages/shared/profile/src/ProfileSelector/useAutoQuerySelector.ts
@@ -48,7 +48,13 @@ export const useAutoQuerySelector = ({
         return;
       }
       dispatch(setAutoQuery('true'));
-      let profileType = profileTypesData.types.find(type => type.name === 'parca_agent_cpu');
+      let profileType = profileTypesData.types.find(type => type.name === 'otel_profiling_agent_on_cpu' && type.delta);
+      if (profileType == null) {
+        profileType = profileTypesData.types.find(type => type.name === 'parca_agent_cpu' && type.delta);
+      }
+      if (profileType == null) {
+        profileType = profileTypesData.types.find(type => type.name === 'process_cpu' && type.delta);
+      }
       if (profileType == null) {
         profileType = profileTypesData.types[0];
       }
