cce8eebc8103f4c608605fba18881bc144bea3f3
Manoj Vivek <p.manoj.vivek@gmail.com>
2024-06-21 13:36:53 +0530

    Unit test and binary embedding fixed

--- ui/packages/app/web/vite.config.ts
+++ ui/packages/app/web/vite.config.ts
@@ -1,11 +1,18 @@
 import react from '@vitejs/plugin-react';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+
+
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: 'build',
   },
 });
