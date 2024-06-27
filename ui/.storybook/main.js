2073171dfc0e5205873b37003d51fd31b0be27a2
Manoj Vivek <p.manoj.vivek@gmail.com>
2024-06-27 14:39:19 +0530

    Storybook vite builder

---
+++ ui/.storybook/main.js
@@ -0,0 +1,7 @@
+export default {
+  stories: ['../src/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  core: {
+    builder: '@storybook/builder-vite', // 👈 The builder enabled here.
+  },
+};
