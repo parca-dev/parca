0b3aff16a4a122e1b85a1807e2ae9fdb90952c29
Manoj Vivek <p.manoj.vivek@gmail.com>
2024-07-16 14:17:39 +0530

    Linter error fixed

--- ui/packages/shared/profile/src/ProfileMetricsGraph/useSumBy.ts
+++ ui/packages/shared/profile/src/ProfileMetricsGraph/useSumBy.ts
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import {useCallback, useEffect, useMemo, useState} from 'react';
 
 
 
