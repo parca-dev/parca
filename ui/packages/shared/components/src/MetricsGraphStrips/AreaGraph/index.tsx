9730e0c544ece28783a3d1ec30853919854e1f2c
Manoj Vivek <p.manoj.vivek@gmail.com>
2024-11-14 15:36:39 +0530

    MetricsGraphStrips component in storybook

---
+++ ui/packages/shared/components/src/MetricsGraphStrips/AreaGraph/index.tsx
@@ -0,0 +1,315 @@
+// Copyright 2022 The Parca Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as d3 from 'd3';
+import cx from 'classnames';
+import { useEffect, useMemo, useRef, useState } from 'react';
+import { Icon } from '@iconify/react';
+
+export interface DataPoint {
+  timestamp: number;
+  value: number;
+}
+
+interface Props {
+  width: number;
+  height: number;
+  marginLeft?: number;
+  marginRight?: number;
+  marginTop?: number;
+  marginBottom?: number;
+  fill?: string;
+  data: DataPoint[];
+  selectionBounds?: [number, number] | undefined;
+  setSelectionBounds: (newBounds: [number, number] | undefined) => void;
+}
+
+const DraggingWindow = ({
+  dragStart,
+  currentX,
+}: {
+  dragStart: number | undefined;
+  currentX: number | undefined;
+}) => {
+  const start = useMemo(() => Math.min(dragStart ?? 0, currentX ?? 0), [dragStart, currentX]);
+  const width = useMemo(() => Math.abs((dragStart ?? 0) - (currentX ?? 0)), [dragStart, currentX]);
+
+  if (dragStart === undefined || currentX === undefined) {
+    return null;
+  }
+
+  return (
+    <div
+      style={{ height: '100%', width, left: start }}
+      className={cx(
+        'bg-gray-500 absolute top-0 opacity-50 border-x-2 border-gray-900 dark:border-gray-100'
+      )}
+    ></div>
+  );
+};
+
+const ZoomWindow = ({
+  zoomWindow,
+  width,
+  onZoomWindowChange,
+  setIsHoveringDragHandle,
+}: {
+  zoomWindow?: [number, number];
+  width: number;
+  onZoomWindowChange: (newWindow: [number, number]) => void;
+  setIsHoveringDragHandle: (arg: boolean) => void;
+}) => {
+  const windowStartHandleRef = useRef<HTMLDivElement>(null);
+  const windowEndHandleRef = useRef<HTMLDivElement>(null);
+  const [zoomWindowState, setZoomWindowState] = useState<[number, number] | undefined>(zoomWindow);
+  const [dragginStart, setDraggingStart] = useState(false);
+  const [draggingEnd, setDraggingEnd] = useState(false);
+
+  useEffect(() => {
+    if (
+      zoomWindow === undefined ||
+      zoomWindowState === undefined ||
+      zoomWindow[0] !== zoomWindowState[0] ||
+      zoomWindow[1] !== zoomWindowState[1]
+    ) {
+      setZoomWindowState(zoomWindow);
+      return;
+    }
+  }, [zoomWindow]);
+
+  if (zoomWindowState === undefined) {
+    return null;
+  }
+  const beforeStart = 0;
+  const beforeWidth = zoomWindowState[0];
+  const afterStart = zoomWindowState[1];
+  const afterWidth = width - zoomWindowState[1];
+
+  return (
+    <div
+      className="absolute w-full h-full"
+      onMouseMove={e => {
+        if (dragginStart) {
+          const [x] = d3.pointer(e);
+          if (x >= afterStart - 10) {
+            return;
+          }
+          const newStart = Math.min(x, afterStart);
+          const newEnd = Math.max(x, afterStart);
+          setZoomWindowState([newStart, newEnd]);
+        }
+        if (draggingEnd) {
+          const [x, y] = d3.pointer(e);
+          if (x <= beforeWidth + 10) {
+            return;
+          }
+          const newStart = Math.min(x, beforeWidth);
+          const newEnd = Math.max(x, beforeWidth);
+          setZoomWindowState([newStart, newEnd]);
+        }
+      }}
+      onMouseLeave={() => {
+        setDraggingStart(false);
+        setDraggingEnd(false);
+      }}
+      onMouseUp={() => {
+        if (dragginStart) {
+          setDraggingStart(false);
+        }
+        if (draggingEnd) {
+          setDraggingEnd(false);
+        }
+        if (zoomWindowState[0] === zoomWindow?.[0] && zoomWindowState[1] === zoomWindow?.[1]) {
+          return;
+        }
+        onZoomWindowChange(zoomWindowState);
+        setZoomWindowState(undefined);
+      }}
+    >
+      <div
+        style={{ height: '100%', width: beforeWidth, left: beforeStart }}
+        className={cx(
+          'bg-gray-500/50 absolute top-0 border-r-2 border-gray-900 dark:border-gray-100 z-20'
+        )}
+      >
+        <div
+          className="w-3 h-4 absolute top-0 right-[-7px] rounded-b bg-gray-200  cursor-ew-resize flex justify-center"
+          onMouseDown={e => {
+            setDraggingStart(true);
+            e.stopPropagation();
+            e.preventDefault();
+          }}
+          ref={windowStartHandleRef}
+          onMouseEnter={() => {
+            setIsHoveringDragHandle(true);
+          }}
+          onMouseLeave={() => {
+            setIsHoveringDragHandle(false);
+          }}
+        >
+          <Icon icon="si:drag-handle-line" className="rotate-90" fontSize={16} />
+        </div>
+      </div>
+
+      <div
+        style={{ height: '100%', width: afterWidth, left: afterStart }}
+        className={cx(
+          'bg-gray-500/50 absolute top-0 border-l-2 border-gray-900 dark:border-gray-100'
+        )}
+      >
+        <div
+          className="w-3 h-4 absolute top-0 rounded-b bg-gray-200 cursor-ew-resize flex justify-center left-[-7px]"
+          onMouseDown={e => {
+            setDraggingEnd(true);
+            e.stopPropagation();
+            e.preventDefault();
+          }}
+          ref={windowEndHandleRef}
+          onMouseEnter={() => {
+            setIsHoveringDragHandle(true);
+          }}
+          onMouseLeave={() => {
+            setIsHoveringDragHandle(false);
+          }}
+        >
+          <Icon icon="si:drag-handle-line" className="rotate-90" fontSize={16} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const AreaGraph = ({
+  data,
+  height,
+  width,
+  marginLeft = 0,
+  marginRight = 0,
+  marginBottom = 0,
+  marginTop = 0,
+  fill = 'gray',
+  selectionBounds,
+  setSelectionBounds,
+}: Props) => {
+  const [mousePosition, setMousePosition] = useState<[number, number] | undefined>(undefined);
+  const [dragStart, setDragStart] = useState<number | undefined>(undefined);
+  const [isHoveringDragHandle, setIsHoveringDragHandle] = useState(false);
+  const isDragging = dragStart !== undefined;
+
+  // Declare the x (horizontal position) scale.
+  const x = d3.scaleUtc(d3.extent(data, d => d.timestamp) as [number, number], [
+    marginLeft,
+    width - marginRight,
+  ]);
+
+  // Declare the y (vertical position) scale.
+  const y = d3.scaleLinear(
+    [0, d3.max(data, d => d.value) as number],
+    [height - marginBottom, marginTop]
+  );
+  const area = d3
+    .area<DataPoint>()
+    .curve(d3.curveMonotoneX)
+    .x(d => x(d.timestamp))
+    .y0(y(0))
+    .y1(d => y(d.value));
+
+  const zoomWindow: [number, number] | undefined = useMemo(() => {
+    if (selectionBounds === undefined) {
+      return undefined;
+    }
+    return [x(selectionBounds[0]), x(selectionBounds[1])];
+  }, [selectionBounds]);
+
+  const setSelectionBoundsWithScaling = ([startPx, endPx]) => {
+    setSelectionBounds([x.invert(startPx).getTime(), x.invert(endPx).getTime()]);
+  };
+
+  return (
+    <div
+      style={{ height, width }}
+      onMouseMove={e => {
+        const [x, y] = d3.pointer(e);
+        setMousePosition([x, y]);
+      }}
+      onMouseLeave={() => {
+        setMousePosition(undefined);
+        setDragStart(undefined);
+      }}
+      onMouseDown={e => {
+        // only left mouse button
+        if (e.button !== 0) {
+          return;
+        }
+
+        // X/Y coordinate array relative to svg
+        const rel = d3.pointer(e);
+
+        const xCoordinate = rel[0];
+        const xCoordinateWithoutMargin = xCoordinate - marginLeft;
+        if (xCoordinateWithoutMargin >= 0) {
+          setDragStart(xCoordinateWithoutMargin);
+        }
+
+        e.stopPropagation();
+        e.preventDefault();
+      }}
+      onMouseUp={e => {
+        if (dragStart === undefined) {
+          return;
+        }
+
+        const rel = d3.pointer(e);
+        const xCoordinate = rel[0];
+        const xCoordinateWithoutMargin = xCoordinate - marginLeft;
+        if (xCoordinateWithoutMargin >= 0 && dragStart != xCoordinateWithoutMargin) {
+          const start = Math.min(dragStart, xCoordinateWithoutMargin);
+          const end = Math.max(dragStart, xCoordinateWithoutMargin);
+          setSelectionBoundsWithScaling([start, end]);
+        }
+        setDragStart(undefined);
+      }}
+      className="relative"
+    >
+      {/* onHover guide, only visible when hovering and not dragging and not having an active zoom window */}
+      <div
+        style={{ height, width: 2, left: mousePosition?.[0] ?? -1 }}
+        className={cx('bg-gray-700/75 dark:bg-gray-200/75 absolute top-0', {
+          hidden: mousePosition === undefined || isDragging || isHoveringDragHandle,
+        })}
+      ></div>
+
+      {/* drag guide, only visible when dragging */}
+      <DraggingWindow dragStart={dragStart} currentX={mousePosition?.[0]} />
+
+      {/* zoom window */}
+      <ZoomWindow
+        zoomWindow={zoomWindow}
+        width={width}
+        onZoomWindowChange={setSelectionBoundsWithScaling}
+        setIsHoveringDragHandle={setIsHoveringDragHandle}
+      />
+
+      {/* Inactive indicator */}
+      <div
+        className={cx('absolute top-0 left-0 w-full h-full bg-gray-900/50 dark:bg-gray-200/50', {
+          hidden: isDragging || selectionBounds !== undefined,
+        })}
+      ></div>
+
+      <svg style={{ width: '100%', height: '100%' }}>
+        <path fill={fill} d={area(data) as string} className="opacity-80" />
+      </svg>
+    </div>
+  );
+};
---
+++ ui/packages/shared/components/src/MetricsGraphStrips/MetricsGraphStrips.stories.tsx
@@ -0,0 +1,56 @@
+// Copyright 2022 The Parca Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { Meta } from '@storybook/react';
+
+import { useArgs } from '@storybook/preview-api';
+
+import { MetricsGraphStrips } from './index';
+import { DataPoint } from './AreaGraph';
+
+const mockData: DataPoint[][] = [[], [], []];
+
+for (let i = 0; i < 200; i++) {
+  for (let j = 0; j < mockData.length; j++) {
+    mockData[j].push({
+      timestamp: 1731326092000 + i * 100,
+      value: Math.floor(Math.random() * 100),
+    });
+  }
+}
+const meta: Meta = {
+  title: 'components/MetricsGraphStrips',
+  component: MetricsGraphStrips,
+};
+export default meta;
+
+export const ThreeCPUStrips = {
+  args: {
+    cpus: Array.from(mockData, (_, i) => `CPU ${i + 1}`),
+    data: mockData,
+    selectedTimeline: { index: 1, bounds: [mockData[0][25].timestamp, mockData[0][100].timestamp] },
+    onSelectedTimeline: (index, bounds) => {
+      console.log('onSelectedTimeline', index, bounds);
+    },
+  },
+  render: function Component(args) {
+    const [, setArgs] = useArgs();
+
+    const onSelectedTimeline = (index, bounds) => {
+      args.onSelectedTimeline(index, bounds);
+      setArgs({ ...args, selectedTimeline: { index, bounds } });
+    };
+
+    return <MetricsGraphStrips {...args} onSelectedTimeline={onSelectedTimeline} />;
+  },
+};
