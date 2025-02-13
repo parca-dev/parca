ed91c845f45e760b9e90a32a1465093fae2495ab
Manoj Vivek <p.manoj.vivek@gmail.com>
2025-02-13 17:28:54 +0530

    Linter fix

--- ui/packages/shared/profile/src/MetricsGraphStrips/index.tsx
+++ ui/packages/shared/profile/src/MetricsGraphStrips/index.tsx
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {useMemo, useState} from 'react';
+import {useState} from 'react';
 
 import {Icon} from '@iconify/react';
 import * as d3 from 'd3';
