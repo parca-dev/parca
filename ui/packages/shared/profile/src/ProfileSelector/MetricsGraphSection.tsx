9488fb526a064cc91f0522c9ce6477db0b822c70
Yomi Eluwande <yomi.eluwande@gmail.com>
2025-04-24 11:36:23 +0200

    ui: Add an Area chart

--- ui/packages/shared/profile/src/ProfileSelector/MetricsGraphSection.tsx
+++ ui/packages/shared/profile/src/ProfileSelector/MetricsGraphSection.tsx
@@ -19,6 +19,7 @@ import {Query} from '@parca/parser';
 
 import {MergedProfileSelection, ProfileSelection} from '..';
 import UtilizationMetricsGraph from '../MetricsGraph/UtilizationMetrics';
+import AreaChart from '../MetricsGraph/UtilizationMetrics/AreaChart';
 import ProfileMetricsGraph, {ProfileMetricsEmptyState} from '../ProfileMetricsGraph';
 import {QuerySelection, type UtilizationMetrics as UtilizationMetricsType} from './index';
 
@@ -137,6 +138,67 @@ export function MetricsGraphSection({
     selectProfile(new MergedProfileSelection(mergeFrom, mergeTo, query));
   };
 
+  const UtilizationGraphToShow = ({
+    utilizationMetrics,
+  }: {
+    utilizationMetrics: Array<{
+      name: string;
+      humanReadableName: string;
+      data: UtilizationMetricsType[];
+    }>;
+  }): JSX.Element => {
+    const throughputMetrics = utilizationMetrics.filter(
+      metric =>
+        metric.name === 'gpu_pcie_throughput_transmit_bytes' ||
+        metric.name === 'gpu_pcie_throughput_receive_bytes'
+    );
+
+    if (utilizationMetrics === undefined || utilizationMetrics.length === 0) {
+      return <></>;
+    }
+
+    return (
+      <div>
+        {utilizationMetrics.map(({name, humanReadableName, data}) => {
+          if (
+            name !== 'gpu_pcie_throughput_transmit_bytes' &&
+            name !== 'gpu_pcie_throughput_receive_bytes'
+          ) {
+            return (
+              <>
+                <UtilizationMetricsGraph
+                  key={name}
+                  data={data}
+                  addLabelMatcher={addLabelMatcher}
+                  setTimeRange={handleTimeRangeChange}
+                  utilizationMetricsLoading={utilizationMetricsLoading}
+                  name={name}
+                  humanReadableName={humanReadableName}
+                  from={querySelection.from}
+                  to={querySelection.to}
+                />
+              </>
+            );
+          }
+          return null;
+        })}
+        {throughputMetrics.length > 0 && (
+          <AreaChart
+            transmitData={throughputMetrics[0].data}
+            receiveData={throughputMetrics[1].data}
+            addLabelMatcher={addLabelMatcher}
+            setTimeRange={handleTimeRangeChange}
+            name={throughputMetrics[0].name}
+            humanReadableName={throughputMetrics[0].humanReadableName}
+            from={querySelection.from}
+            to={querySelection.to}
+            utilizationMetricsLoading={utilizationMetricsLoading}
+          />
+        )}
+      </div>
+    );
+  };
+
   return (
     <div className={cx('relative', {'py-4': !showMetricsGraph})}>
       {setDisplayHideMetricsGraphButton != null ? (
@@ -159,21 +221,7 @@ export function MetricsGraphSection({
             querySelection.to !== undefined ? (
               <>
                 {utilizationMetrics !== undefined ? (
-                  utilizationMetrics.map(({name, humanReadableName, data}) => (
-                    <>
-                      <UtilizationMetricsGraph
-                        key={name}
-                        data={data}
-                        addLabelMatcher={addLabelMatcher}
-                        setTimeRange={handleTimeRangeChange}
-                        utilizationMetricsLoading={utilizationMetricsLoading}
-                        name={name}
-                        humanReadableName={humanReadableName}
-                        from={querySelection.from}
-                        to={querySelection.to}
-                      />
-                    </>
-                  ))
+                  <UtilizationGraphToShow utilizationMetrics={utilizationMetrics} />
                 ) : (
                   <>
                     <ProfileMetricsGraph
