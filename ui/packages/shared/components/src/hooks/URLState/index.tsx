9a38ff7a1523589f1acdff951d47a593f489247e
Manoj Vivek <p.manoj.vivek@gmail.com>
2025-08-06 21:41:38 +0530

    Handled the empty arrays properly

--- ui/packages/shared/components/src/hooks/URLState/index.tsx
+++ ui/packages/shared/components/src/hooks/URLState/index.tsx
@@ -140,7 +140,7 @@ export const useURLStateCustom = <T,>(
   const [urlValue, setURLValue] = useURLState<string>(param, _options);
 
   const val = useMemo<T>(() => {
-    if (urlValue === undefined) {
+    if (urlValue == null || (Array.isArray(urlValue) && urlValue.length === 0)) {
       return undefined as T;
     }
     return parse(urlValue);
