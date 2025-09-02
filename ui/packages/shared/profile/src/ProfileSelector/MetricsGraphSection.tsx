d448997840466b0c870b5874420d30ccdca9d8d6
Manoj Vivek <p.manoj.vivek@gmail.com>
2025-09-02 17:11:57 +0530

    Fixed the timerange selection bug during the click-n-drag interaction

--- ui/packages/shared/profile/src/ProfileSelector/MetricsGraphSection.tsx
+++ ui/packages/shared/profile/src/ProfileSelector/MetricsGraphSection.tsx
@@ -76,7 +76,7 @@ export function MetricsGraphSection({
     const to = range.getToMs();
     let mergedProfileParams = {};
     if (query.profileType().delta) {
-      mergedProfileParams = {mergeFrom: from, mergeTo: to};
+      mergedProfileParams = { mergeFrom: from * 1_000_000, mergeTo: to * 1_000_000 };
     }
     setTimeRangeSelection(range);
     selectQuery({
