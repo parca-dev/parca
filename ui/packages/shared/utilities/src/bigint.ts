b8c5688ad06969e51a253d898d56421da103a97d
Manoj Vivek <p.manoj.vivek@gmail.com>
2026-04-17 21:42:37 +0530

    Defensive guards to prevent ending up with Infinity (#6335)

--- ui/packages/shared/utilities/src/bigint.ts
+++ ui/packages/shared/utilities/src/bigint.ts
@@ -55,8 +55,9 @@ export const scaleLinear = (
     console.log('domainRange', domainRange, rangeRange, divide(rangeRange, domainRange));
   }
 
-  // rate * MULTIPLE to retain the decimal places in BigInt format, then divide by MULTIPLE to get the final result
-  const rate = BigInt(Math.round(divide(rangeRange, domainRange) * MULTIPLE));
+  // guard 0n to avoid BigInt(Infinity).
+  const rate =
+    domainRange === 0n ? 0n : BigInt(Math.round(divide(rangeRange, domainRange) * MULTIPLE));
 
   const func = (x: bigint): number => {
     if (debugLog) {
@@ -74,6 +75,9 @@ export const scaleLinear = (
   };
 
   func.ticks = (count = 5): bigint[] => {
+    if (count <= 1 || domainRange === 0n) {
+      return [domainMin];
+    }
     const step = domainRange / BigInt(count - 1);
     const ticks: bigint[] = [];
     for (let i = 0; i < count; i++) {
