51307d8f156d9cf9bcedeb2fc0cb7db0498f99be
HSP18SCM23P <38229980+HSP18SCM23P@users.noreply.github.com>
2026-09-24 21:19:34 -0700

    Use mmap-based DWARF loading in DwarfLiner

--- pkg/symbol/addr2line/dwarf.go
+++ pkg/symbol/addr2line/dwarf.go
@@ -11,9 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 package addr2line
-
 
 import (
 	"context"
@@ -22,21 +20,17 @@ import (
 	"fmt"
 	"runtime/debug"
 
-
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
-
 
 	"github.com/parca-dev/parca/pkg/profile"
 	"github.com/parca-dev/parca/pkg/symbol/demangle"
 	"github.com/parca-dev/parca/pkg/symbol/elfutils"
 )
 
-
 // DwarfLiner is a symbolizer that uses DWARF debug info to symbolize addresses.
 type DwarfLiner struct {
 	logger log.Logger
-
 
 	debugData *dwarf.Data
 	dbgFile   elfutils.DebugInfoFile
@@ -48,14 +42,12 @@ type DwarfLiner struct {
 	unmapDWARF func() error
 }
 
-
 // DWARF creates a new DwarfLiner.
 func DWARF(logger log.Logger, filename string, f *elf.File, demangler *demangle.Demangler) (*DwarfLiner, error) {
 	debugData, unmapDWARF, err := elfutils.LoadDWARFData(f, filename)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read DWARF data: %w", err)
 	}
-
 
 	dbgFile, err := elfutils.NewDebugInfoFile(debugData, demangler)
 	if err != nil {
@@ -65,7 +57,6 @@ func DWARF(logger log.Logger, filename string, f *elf.File, demangler *demangle.
 		}
 		return nil, err
 	}
-
 
 	return &DwarfLiner{
 		logger:     log.With(logger, "liner", "dwarf"),
@@ -77,7 +68,6 @@ func DWARF(logger log.Logger, filename string, f *elf.File, demangler *demangle.
 	}, nil
 }
 
-
 func (dl *DwarfLiner) Close() error {
 	// Release the mmap-backed DWARF sections, if any, before closing the file.
 	var unmapErr error
@@ -88,4 +78,59 @@ func (dl *DwarfLiner) Close() error {
 		return err
 	}
 	return unmapErr
+}
+
+func (dl *DwarfLiner) File() string {
+	return dl.filename
+}
+
+func (dl *DwarfLiner) PCRange() ([2]uint64, error) {
+	r := dl.debugData.Reader()
+
+	minSet := false
+	var min, max uint64
+	for {
+		e, err := r.Next()
+		if err != nil {
+			return [2]uint64{}, fmt.Errorf("read DWARF entry: %w", err)
+		}
+		if e == nil {
+			break
+		}
+
+		ranges, err := dl.debugData.Ranges(e)
+		if err != nil {
+			return [2]uint64{}, err
+		}
+		for _, pcs := range ranges {
+			if !minSet {
+				min = pcs[0]
+				minSet = true
+			}
+			if pcs[1] > max {
+				max = pcs[1]
+			}
+			if pcs[0] < min {
+				min = pcs[0]
+			}
+		}
+	}
+
+	return [2]uint64{min, max}, nil
+}
+
+// PCToLines returns the resolved source lines for a program counter (memory address).
+func (dl *DwarfLiner) PCToLines(ctx context.Context, addr uint64) (lines []profile.LocationLine, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			level.Debug(dl.logger).Log("msg", "recovered stack trace", "trace", string(debug.Stack()))
+			err = fmt.Errorf("recovering from panic in DWARF add2line: %v", r)
+		}
+	}()
+
+	lines, err = dl.dbgFile.SourceLines(addr)
+	if err != nil {
+		return nil, err
+	}
+	return lines, nil
 }
